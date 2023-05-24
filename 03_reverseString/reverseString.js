110d18108396642616b24e28ad33b43c16e47540
Quincy Thai <quincythai50@icloud.com>
2023-05-23 19:37:49 -0700

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+/* Takes in a string and reverses it
+EX: reverseString("hello there") returns "ereht olleh"
+*/
 
+const reverseString = function(inputString) {
+  // cannot mutate strings in js
+  // Algorithm: reverse iteration appending chars to result string
+  let str = "";
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    str += inputString[i];
+  }
+  return str;
 };
 
 // Do not edit below this line
