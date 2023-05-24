dcfbf0b4c2cca5fff2beff4311f690e9bf015ca0
Quincy Thai <quincythai50@icloud.com>
2023-05-23 19:23:32 -0700

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+/* Take in string and number of repetitions, return string x # of repetitions 
+EX: repeatString('hey', 3) -> 'heyheyhey'
+*/
+const repeatString = function(str, numRepeat) {
+  if (numRepeat < 0)
+    return 'ERROR';
 
+  let result = "";
+  for (let i = 0; i < numRepeat; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
