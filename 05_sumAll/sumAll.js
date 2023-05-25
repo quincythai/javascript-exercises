23bdcf125c15b1d358960d9ce8384c6f0d2300aa
Quincy Thai <quincythai50@icloud.com>
2023-05-24 17:58:29 -0700

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+/* Returns the sum of all numbers between a and b inclusive 
+EX: sumAll(1, 4) -> 1 + 2 + 3 + 4 = 10
 
+*/
+
+const sumAll = function(a, b) {
+  if (a < 0 || b == 0 || typeof(a) != "number" || typeof(b) != "number")
+    return "ERROR";
+
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
