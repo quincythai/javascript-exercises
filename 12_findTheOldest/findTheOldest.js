d19c0fef2c69d8cda4a3216e0684f80ed7b80cf4
Quincy Thai <quincythai50@icloud.com>
2023-07-10 16:49:26 -0700

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,37 @@
-const findTheOldest = function() {
+// Given array full of objects with name, yearOfBirth, and yearOfDeath, return the oldest person object.
+const findTheOldest = function(people) {
+  // let oldest = 0;
+  // let oldestPerson = null;
 
+  // people.forEach(person => {
+  //   let age;
+  //   if ("yearOfDeath" in person) {
+  //     age = person.yearOfDeath - person.yearOfBirth;
+  //   } else {
+  //     age = (new Date().getFullYear() - person.yearOfBirth);
+  //   }
+
+  //   if (age > oldest) {
+  //     oldest = age;
+  //     oldestPerson = person;
+  //   }
+  // });
+  // return oldestPerson;
+
+  return people.reduce((oldestPerson, currentPerson) => {
+    if (getAge(oldestPerson) < getAge(currentPerson)) {
+      oldestPerson = currentPerson;
+    }
+    return oldestPerson;
+  });
+
+  function getAge(person) {
+    if ("yearOfDeath" in person) {
+      return person.yearOfDeath - person.yearOfBirth;
+    } else {
+      return new Date().getFullYear() - person.yearOfBirth;
+    }
+  }
 };
 
 // Do not edit below this line
