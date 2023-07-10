08782acd0bb6322f8672c546c5cfd77b743a169d
Quincy Thai <quincythai50@icloud.com>
2023-07-10 15:38:16 -0700

    Complete fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n <= 0)
+    return "OOPS";
+  if (n <= 2)
+    return 1;
+  let a = 1;
+  let b = 1;
+  let sum = 0;
+  for (let i = 0; i < n - 2; i++) {
+    sum = a + b;
+    a = b;
+    b = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
