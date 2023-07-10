134a9a11497676556033330893773d7afdd548a8
Quincy Thai <quincythai50@icloud.com>
2023-07-10 15:17:17 -0700

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const a = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""); // Removes punctuation + spaces, and makes lowercase.
+  return a.split("").reverse().join("") === a; // Split to char array, reverse it, and recombine array into string.
 };
 
 // Do not edit below this line
