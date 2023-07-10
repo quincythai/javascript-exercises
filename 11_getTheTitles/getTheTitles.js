95b7fc88bf91db7b23fee95780ec5554a13e02db
Quincy Thai <quincythai50@icloud.com>
2023-07-10 15:52:29 -0700

    Add alternate solution to 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,12 @@
 const getTheTitles = function(arr) {
-  let bookTitles = [];
-  arr.forEach(obj => {
-    bookTitles.push(obj.title);
-  });
-  return bookTitles;
+  // let bookTitles = [];
+  // arr.forEach(obj => {
+  //   bookTitles.push(obj.title);
+  // });
+  // return bookTitles;
+
+  let result = arr.map(obj => obj.title);
+  return result;
 }
 
 // Do not edit below this line
